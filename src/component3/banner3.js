60b642f350be7b93a644e07b1739c214a0c7a2a0
Arthiclaks <chitraraveendran.com>
2023-06-03 17:56:11 +0530

    comp3 updated

---
+++ src/component3/banner3.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Banner3(){
+    return(
+        <>
+        
+        </>
+    );
+}
---
+++ src/component3/comp3_main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Banner3 from './banner3';
+
+function Component3(){
+    return(
+        <Banner3/>
+    );
+}
