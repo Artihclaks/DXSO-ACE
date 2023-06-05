a75ac78fbcb7a70d168616ece693422c1779cfcc
Sasikala Kavignanam <sasikalakavignanam@gmail.com>
2023-06-05 12:11:28 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Order from './component0/basicplan';
 import {Routes, Route} from 'react-router-dom';
 import Table from './component2/table2';
-import Leftcolumn from './component3/leftbox';
 import Lbox from './component4/lbox';
 import Browse from './component0/browse';
 import Order1 from './component1/Order';
+import Comp from './component/comp_main'
 
 function App() {
   return (
     <div className='App'>
     <Routes>
       <Route path='/' element={<Order/>}></Route>
+      <Route path='/bom' element={<Comp/>}></Route>
       <Route path='/planner' element={<Order1/>}></Route>
      <Route path='operator' element={<Table/>}></Route>
-     <Route path='inspection' element={<Leftcolumn/>}></Route>
      <Route path='inspector' element={<Lbox/>}></Route>
      </Routes>
      </div>
